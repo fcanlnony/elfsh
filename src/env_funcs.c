8ec400048d76ee8332e21889089a518ba97de3f9
fcanlnony <1923170329@qq.com>
2022-11-19 23:28:29 +0800

    update env_funcs.c

--- src/env_funcs.c
+++ src/env_funcs.c
@@ -14,15 +14,13 @@ short check_env_cmd(char *input)
 
 void run_env_cmd(char **argv)
 {
-    if(strcmp(argv[0],":env") == 0)
-    {
+    if(strcmp(argv[0],":env") == 0) {
 	if(strcmp(argv[2],"show") == 0) {
-		if(strcmp(argv[3],"NULL") == 0)
-		    printf("%s\n",getenv(argv[1]));
-	} else _exit(0);
+	    if(strcmp(argv[3],"NULL") == 0)
+		printf("%s\n",getenv(argv[1]));
+	} else if(strcmp(argv[2],"add") == 0) {
+	    if(strcmp(argv[4],"NULL") == 0)
+		setenv(argv[1],argv[3],1);
+	}
     }
-    else if(strcmp(argv[2],"add") == 0) {
-	if(strcmp(argv[4],"NULL") == 0)
-	    setenv(argv[1],argv[3],1);
-    } else _exit(0);
 }
